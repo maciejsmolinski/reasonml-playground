8bb53b87d2ff3ab9d50ebf97f53c6a701c34b2ee
Maciej Smolinski <contact@maciejsmolinski.com>
2018-05-15 08:51:13 +0200

    Application: Print details of a single category in console whilst developing the functionality.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import * as Main from "./main.bs";
+import * as Api from "./utils/api.bs";
 
 Main.init();
+
+Api.getCategory("javascript")
+  .then(data => console.log("⌗ Data Received:\n", data))
+  .catch(err => console.error("⌗ Error Received:\n", err));
